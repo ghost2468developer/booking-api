711991a3f2aca6122cf54641e32f16ee75b92b1d
Kenneth Siyabonga Ncube <160495796+ghost2468developer@users.noreply.github.com>
2026-04-24 14:03:08 +0200

    feat: get available time slot route

--- src/booking/routes.js
+++ src/booking/routes.js
@@ -5,6 +5,7 @@ const auth = require("../middleware/auth")
 // USER routes
 router.post("/", auth, ctrl.createBooking)
 router.get("/mine", auth, ctrl.getMyBookings)
+router.get("/available-slots", auth, ctrl.getAvailableSlots)
 
 // MECHANIC routes
 router.get("/mechanic", auth, ctrl.getMechanicBookings)
