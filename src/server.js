c76a7599115aed6b8c1056d2870ab570093d3e80
Kenneth Siyabonga Ncube <160495796+ghost2468developer@users.noreply.github.com>
2026-04-24 13:25:05 +0200

    feat: spell error

--- src/server.js
+++ src/server.js
@@ -7,16 +7,21 @@ const bookingRoutes = require("./booking/routes")
 
 const app = express()
 
-app.use("/api/auth", authRoutes)
-app.use("/api/bookings")
+// middleware
+app.use(cors())
+app.use(express.json())
 
-// Health Check
+// routes
+app.use("/api/auth", authRoutes)
+app.use("/api/bookings", bookingRoutes)
+
+// health check
 app.get("/", (req, res) => {
-    res.json({ message: "API is running 🚀"})
+  res.json({ message: "API is running 🚀" })
 })
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-    console.log('Server running on port ${PORT}')
+  console.log(`Server running on port ${PORT}`)
 })
