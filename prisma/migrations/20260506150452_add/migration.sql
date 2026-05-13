5340225e27b9cdbf8621184dde62e90f575311f6
Kenneth Siyabonga Ncube <160495796+ghost2468developer@users.noreply.github.com>
2026-05-13 10:46:36 +0200

    reset

--- prisma/migrations/20260506150452_add/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "endHour" TEXT,
-ADD COLUMN     "startHour" TEXT,
-ADD COLUMN     "workingDays" TEXT;
--- src/user/routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router()
-const ctrl = require("./controllers")
-
-router.post("/register", ctrl.register)
-router.post("/login", ctrl.login)
-router.get("/mechanics", ctrl.getMechanics)
-router.patch("/:id/complete", auth, ctrl.completeBooking)
-
-module.exports = router
